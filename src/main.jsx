f935ba1463049e2a3bbd0f99907f6bed2dc288df
szparadyse <axelgodart17@gmail.com>
2025-02-13 11:53:03 +0100

    feat : sidebar component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+import Layout  from '@/pages/Layout'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Page from './app/dashboard/page'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Page/>}>
+          <Route path='/' element={<p>hello</p>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
