aaa2f476ad9060538b95102610ac093dd21c16ad
szparadyse <axelgodart17@gmail.com>
2025-02-13 10:41:55 +0100

    Premier commit - Projet Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import  './components/ui/button'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <Button>
+      test btn 
+    </Button>
+    </>
+  )
+}
+
+export default App
